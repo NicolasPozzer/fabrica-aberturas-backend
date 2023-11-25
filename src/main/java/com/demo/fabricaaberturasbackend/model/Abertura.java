abf0e82dfe0b7f4fdf09813fe1332fd462a333e9
NicolasPozzer <pozzernico@gmail.com>
2023-11-25 02:49:36 -0300

    add security and fix

--- src/main/java/com/demo/fabricaaberturasbackend/model/Abertura.java
+++ src/main/java/com/demo/fabricaaberturasbackend/model/Abertura.java
@@ -1,6 +1,6 @@
 package com.demo.fabricaaberturasbackend.model;
 
-import jakarta.persistence.*;
+import javax.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,26 +11,25 @@ public class Abertura {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
-    /*private String nombre;*/
+    private String nombre;
     private String tipo;
-    /*private String detalles;
+    private String detalles;
     private String medidas;
-    private Double precio;*/
+    private Double precio;
+    private String urlMiniatura;
+    private String urlFoto;
 
     public Abertura() {
     }
 
-    /*public Abertura(Long id, String nombre, String tipo, String detalles, String medidas, Double precio) {
+    public Abertura(Long id, String nombre, String tipo, String detalles, String medidas, Double precio, String urlMiniatura, String urlFoto) {
         this.id = id;
         this.nombre = nombre;
         this.tipo = tipo;
         this.detalles = detalles;
         this.medidas = medidas;
         this.precio = precio;
-    }*/
-
-    public Abertura(Long id, String tipo) {
-        this.id = id;
-        this.tipo = tipo;
+        this.urlMiniatura = urlMiniatura;
+        this.urlFoto = urlFoto;
     }
 }
